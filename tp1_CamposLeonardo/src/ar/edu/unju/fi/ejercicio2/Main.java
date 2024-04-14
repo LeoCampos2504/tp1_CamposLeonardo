870dd081a11eea2563ce003301125eb0fa933699
eltig <eltig@Leo-Campos>
2024-04-13 21:15:59 -0300

    adjunto ejercicio 2

---
+++ tp1_CamposLeonardo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais="Argentina";
+		int edad=18;
+		double altura=18.42;
+		double precio=32.45;
+		String numerotel="3886514323";
+		double coseno=Math.cos(0.5);
+		System.out.println(pais);
+		System.out.println(edad);
+		System.out.println(altura);
+		System.out.println(precio);
+		System.out.println(numerotel);
+		System.out.println(coseno);
+	
+	}
+
+}
