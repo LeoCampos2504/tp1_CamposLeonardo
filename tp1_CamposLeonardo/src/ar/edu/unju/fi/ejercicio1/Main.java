d08bad6f8305326e1bd73aa1cbc58241b6b3364e
eltig <eltig@Leo-Campos>
2024-04-13 21:14:35 -0300

    adjunto ejercicio 1

---
+++ tp1_CamposLeonardo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=5;
+		int c=23;
+		int d=12;
+		int e=8;
+		double prom=(a+b+c+d+e)/5;
+		System.out.println("El promedio es: "+prom);
+	}
+
+}
